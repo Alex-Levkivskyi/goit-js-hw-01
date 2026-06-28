3d2026a5cb4140881afd7e13548f3cb6e58a88c3
Alex_L <alexandrrbatkovich@gmail.com>
2026-06-27 17:06:40 -0700

    task-3

--- js/task-3.js
+++ js/task-3.js
@@ -1,5 +1,8 @@
 function getElementWidth(content, padding, border) {
-    // ...
+    const contentNum = Number.parseFloat(content);
+    const paddingNum = Number.parseFloat(padding);
+    const borderNum = Number.parseFloat(border);
+    return contentNum + paddingNum * 2 + borderNum * 2;
 }
 
 console.log(getElementWidth("50px", "8px", "4px")); // 74
